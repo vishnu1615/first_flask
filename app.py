566a6b227cc07666e67fd0200b41b8ab2b2a16d9
vishnu1615 <vishnureddy1615@gmail.com>
2025-04-20 21:38:49 +0530

    second

--- app.py
+++ app.py
@@ -5,6 +5,9 @@ app = Flask(__name__)
 @app.route('/')
 def first():
     return "hello vishnu"
+@app.route('/vishnu')
+def second():
+    return 'this is second form'
 
 if __name__ == '__main__':
     app.run()
